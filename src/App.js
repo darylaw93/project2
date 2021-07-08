5436051b63602eb5f9a5087fd49a4ed8ee73a1fe
Darylaw93 <darylaw93@gmail.com>
2021-07-08 16:40:29 +0800

    hopefully no cors error5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import CoinList from "./components/CoinList";
-import CoinList2 from "./components/CoinList2";
+// import CoinList2 from "./components/CoinList2";
 import { Route, Link, Redirect, Switch } from "react-router-dom";
 import MarketCap from "./components/MarketCap";
 import Coin from "./components/Coin";
 
 const coinURL ="https://api.nomics.com/v1/currencies/ticker?key=90cbf49ed7f5579b5b8c18dc354c3749d20705c9&ids=&interval=&convert=USD&per-page=100&page=1";
-const coinURL2 ="https://api.nomics.com/v1/currencies/ticker?key=90cbf49ed7f5579b5b8c18dc354c3749d20705c9&ids=&interval=&convert=USD&per-page=100&page=2";
+// const coinURL2 ="https://api.nomics.com/v1/currencies/ticker?key=90cbf49ed7f5579b5b8c18dc354c3749d20705c9&ids=&interval=&convert=USD&per-page=100&page=2";
 const mcapURL = "https://api.coinlore.net/api/global/";
 
 function App() {
   const [coinList, setCoinList] = useState();
-  const [coinList2, setCoinList2] = useState();
+  // const [coinList2, setCoinList2] = useState();
   const [marketCap, setMarketCap] = useState(0);
 
   useEffect(() => {
@@ -27,17 +27,17 @@ function App() {
       .catch((error) => console.error(error));
   }, [setCoinList]);
 
-  useEffect(() => {
-    fetch(coinURL2, {mode: "no-cors"})
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Bad Response");
-      })
-      .then((data) => setCoinList2(data))
-      .catch((error) => console.error(error));
-  }, [setCoinList2]);
+  // useEffect(() => {
+  //   fetch(coinURL2, {mode: "no-cors"})
+  //     .then((res) => {
+  //       if (res.ok) {
+  //         return res.json();
+  //       }
+  //       throw new Error("Bad Response");
+  //     })
+  //     .then((data) => setCoinList2(data))
+  //     .catch((error) => console.error(error));
+  // }, [setCoinList2]);
 
   useEffect(() => {
     fetch(mcapURL)
@@ -64,9 +64,9 @@ function App() {
           <Route exact path="/">
             <CoinList data={coinList} />
           </Route>
-          <Route path="/page2">
+          {/* <Route path="/page2">
             <CoinList2 data={coinList2} />
-          </Route>
+          </Route> */}
           <Route path="/id/:id/:name">
             <Coin />
           </Route>
